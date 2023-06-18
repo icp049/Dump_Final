eef33fa4116b51ecc1803cd5f4205859fa1a0ae6
icp049 <ianpepe2000@gmail.com>
2023-06-18 01:06:21 -0600

    just fixed the upload in database, can't fix retrieval in srapbookview

--- Dump Final/Views/ScrapBookView.swift
+++ Dump Final/Views/ScrapBookView.swift
@@ -13,27 +13,14 @@ struct ScrapBookView: View {
                 UploadView(newItemPresented: $viewModel.showingNewItemView)
             }
             
-            if let photos = viewModel.photos {
-                LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 10), count: 3), spacing: 10) {
-                    ForEach(photos, id: \.id) { photo in
-                        Image(uiImage: photo.image)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(maxHeight: 200)
-                    }
-                }
-                .padding(.horizontal)
-            } else {
-                ProgressView() // Show a loading indicator while photos are being fetched
-            }
+            .padding()
         }
-        .padding()
     }
-}
-
-struct ScrapBookView_Previews: PreviewProvider {
-    static var previews: some View {
-        ScrapBookView()
+    
+    struct ScrapBookView_Previews: PreviewProvider {
+        static var previews: some View {
+            ScrapBookView()
+        }
     }
+    
 }
-
