f24aa60add0ee73e665c2995da4e48ba22fe9b1f
icp049 <ianpepe2000@gmail.com>
2023-06-22 09:26:48 -0600

    did the following and followers functionality and logic

--- Dump Final/ViewModels/UserProfileViewViewModel.swift
+++ Dump Final/ViewModels/UserProfileViewViewModel.swift
@@ -6,15 +6,61 @@ class UserProfileViewModel: ObservableObject {
     
     func followUser(userId: String, currentUserId: String) {
         let currentUserFollowingRef = db.collection("users").document(currentUserId).collection("following").document(userId)
+        let followedUserFollowersRef = db.collection("users").document(userId).collection("followers").document(currentUserId)
         
-        currentUserFollowingRef.setData(["userId": userId]) { error in
+        currentUserFollowingRef.getDocument { snapshot, error in
             if let error = error {
                 // Handle the error
-                print("Error following user: \(error.localizedDescription)")
+                print("Error checking following status: \(error.localizedDescription)")
+                return
+            }
+            
+            if snapshot?.exists == true {
+                // User is already following, so unfollow them
+                currentUserFollowingRef.delete { error in
+                    if let error = error {
+                        // Handle the error
+                        print("Error unfollowing user: \(error.localizedDescription)")
+                    } else {
+                        // Unfollow action completed successfully
+                        print("Successfully unfollowed user!")
+                    }
+                }
+                
+                // Remove current user from the follower list of the user being unfollowed
+                followedUserFollowersRef.delete { error in
+                    if let error = error {
+                        // Handle the error
+                        print("Error removing follower: \(error.localizedDescription)")
+                    } else {
+                        // Follower removed successfully
+                        print("Successfully removed follower!")
+                    }
+                }
             } else {
-                // Follow action completed successfully
-                print("Successfully followed user!")
+                // User is not following, so follow them
+                currentUserFollowingRef.setData(["userId": userId]) { error in
+                    if let error = error {
+                        // Handle the error
+                        print("Error following user: \(error.localizedDescription)")
+                    } else {
+                        // Follow action completed successfully
+                        print("Successfully followed user!")
+                        
+                        // Add current user to the follower list of the user being followed
+                        followedUserFollowersRef.setData(["followerId": currentUserId]) { error in
+                            if let error = error {
+                                // Handle the error
+                                print("Error adding follower: \(error.localizedDescription)")
+                            } else {
+                                // Follower added successfully
+                                print("Successfully added follower!")
+                            }
+                        }
+                    }
+                }
             }
         }
     }
+
 }
