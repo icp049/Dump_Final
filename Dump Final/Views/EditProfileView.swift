05cedced2c3419e5bf0dbafb55b6cb3f623d0c84
icp049 <ianpepe2000@gmail.com>
2023-07-16 17:06:19 -0600

    doing textfields and upload functionality of profile view photo

--- Dump Final/Views/EditProfileView.swift
+++ Dump Final/Views/EditProfileView.swift
@@ -11,6 +11,9 @@ struct EditProfileView: View {
     
     @StateObject var viewModel = EditProfileController()
     @Binding var newItemPresented: Bool
+    @State private var showImagePicker = false
+    @State private var selectedImage: UIImage?
+
     
     var body: some View {
         VStack{
@@ -18,6 +21,50 @@ struct EditProfileView: View {
                 .font(.system(size:32))
                 .bold()
                 .padding(.top, 100)
+            
+            ZStack {
+                Circle()
+                    .foregroundColor(Color.blue)
+                    .frame(width: 125, height: 125)
+                
+                if let image = selectedImage {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .clipShape(Circle())
+                        .frame(width: 120, height: 120)
+                } else {
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color.blue)
+                        .frame(width: 125, height: 125)
+                }
+                
+                Button(action: {
+                    showImagePicker = true
+                }) {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(Color.white)
+                        .frame(width: 30, height: 30)
+                        .background(Color.blue)
+                        .clipShape(Circle())
+                        .padding(4)
+                        .offset(x: 45, y: 45)
+                }
+                .sheet(isPresented: $showImagePicker) {
+                    ImagePicker1(sourceType: .photoLibrary) { image in
+                        selectedImage = image
+                    }
+                }
+            }
+            
+            
+            // add textfields here for username and name change 
+            
+           
             
             
             RUButton(title: "Save Changes", background: .green)
